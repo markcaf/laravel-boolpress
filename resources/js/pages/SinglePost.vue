834d131c8892660d03ad2cafedc566648a868f26
markcaf <marcocafassog@gmail.com>
2022-09-29 18:26:00 +0200

    feat(pages): show single post with a dynamic route

---
+++ resources/js/pages/SinglePost.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col my-3">
+            <div class="post">
+                <div class="row justify-content-center">
+                    <PostCard :key="post.id" :post="post" />
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import PostCard from '../components/PostCard.vue';
+import axios from 'axios';
+
+export default {
+
+    components: {
+        PostCard,
+    },
+
+    data: function(){
+        return{
+            post: {},
+            loading: false,
+        }
+    },
+
+    methods: {
+
+        getPosts(){
+            const id = this.$route.params.id;
+
+            axios.get(`/api/posts/${id}`).then((response) => {
+                this.post = response.data.results.data;
+                this.loading = false;
+            }).catch((error) => {
+                console.error(error);
+            })
+        }
+    },
+
+    created(){
+        this.getPosts();
+    }
+};
+</script>
+
+<style lang='scss' scoped>
+
+</style>
