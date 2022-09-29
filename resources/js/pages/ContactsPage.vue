f6f743f37345640a947a552ae6b52b16e95654c4
markcaf <marcocafassog@gmail.com>
2022-09-29 17:31:04 +0200

    feat(js\pages): add pages as routes for vue-router

---
+++ resources/js/pages/ContactsPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Contacts</h3>
+                <ul>
+                    <li>Phone: 3283375947</li>
+                    <li>Email: prova@email.it</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactsPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import MainComponent from '../components/Main.vue';
+    export default {
+        name: 'HomePage',
+        components: {
+            MainComponent
+        }
+    }
+</script>
+
+<style>
+
+</style>
