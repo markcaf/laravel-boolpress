315ef9af537100d644331b530a316804db07ebe7
markcaf <marcocafassog@gmail.com>
2022-09-29 17:30:12 +0200

    feat(js): add import of vue-router in front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,10 +1,16 @@
 window.Vue = require('vue');
+import Vue from 'vue';
 
 // importiamo il componente App
 import App from './views/App';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter)
+
+import router from './router.js';
 
 const app = new Vue({
     el: '#root',
-
-    render: h => h(App) //mostriamo App all'avvio di Vue
+    render: h => h(App), //mostriamo App all'avvio di Vue
+    router
 });
